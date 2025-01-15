7d9c30e9e383621cf9303b352f7d792cca6d0b0c
MilinVaniyawala <milinvaniyawala@gmail.com>
2025-01-15 10:18:34 -0500

    Add project files.

---
+++ Week2_Hello_World/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc; 
+using System.Text.Encodings.Web;
+
+namespace Week2_Hello_World.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld
+        public string Index()
+        {
+            return "This is default's action...";
+        }
+
+        //
+        // GET: /HelloWorld/Welcome
+        // REQUIRES 'using System.Text.Encodings.Web;'
+        public string Welcome(string name,int ID = 1)
+        {
+            // return "Welcome method..";
+            // return HtmlEncoder.Default.Encode($"Hello {name}, Number of Times is: {numTimes}");
+            return HtmlEncoder.Default.Encode($"Hello {name},ID: {ID}");
+        }
+    }
+}
---
+++ Week2_Hello_World/README.txt
@@ -0,0 +1,20 @@
+﻿2025-01-18
+0845
+VS Community 2022 17.12.0
+.NET SDK 8.0.404
+
+0846
+- Created the new HelloWorld Application
+0850
+- Run and Test the application, however it's works properly
+0908
+- Created HelloWorldController
+	- Add 2 methods (Index and Welcome)
+0923
+- In Controller Updated the Welcome Method (two paramters:name and numTimes)
+0925
+- Tested Updated Method, Also did some code testing with my own values
+0932
+- In Controller Update the Welcome Method (two parameters:name and ID)
+0933
+- Also Testing the created method and confirm it works.
